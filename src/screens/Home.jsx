0f1158c84ce8b497a23c4663c62943daf6644c8e
Nickemolmedo <nickolasolmedo2903@gmail.com>
2024-06-23 22:49:01 -0300

    Avances en front , diseño y funcionalidad

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,26 +5,50 @@ import iconoAsignacion from '../assets/img/icono-asignacion.png'
 import iconoDepartamento from '../assets/img/icono-departamento.png'
 import iconoEmpleado from '../assets/img/icono-empleado.png'
 import iconoProyecto from '../assets/img/icono-proyecto.png'
+import iconoVolver from '../assets/img/icono-volver.png'
 
 
 export const Home = () => {
-  return (
-    <HomeScreen>
+    return (
+        <HomeScreen>
+            <CardContainer>
+                <Card image={iconoEmpleado} text="Empleados" link="" />
+            </CardContainer>
+            <CardContainer>
+                <Card image={iconoEmpleado} text="Empleados" link="" />
+            </CardContainer>
+            <CardContainer>
+                <Card image={iconoDepartamento} text="Departamentos" link="" />
+            </CardContainer>
+            <CardContainer>
+                <Card image={iconoProyecto} text="Proyectos" link="" />
+            </CardContainer>
+            <CardContainer>
+                <Card image={iconoAsignacion} text="Asignaciones" link="" />
+            </CardContainer>
+            <CardContainer>
+                <Card image={iconoVolver} text="Volver" link="" />
+            </CardContainer>
 
-<Card image={iconoEmpleado} text="Empleados" link=""/>
-<Card image={iconoDepartamento} text="Departamentos" link=""/>
-<Card image={iconoProyecto} text="Proyectos" link=""/>
-<Card image={iconoAsignacion} text="Asignaciones" link=""/>
-
-        
-    </HomeScreen>
-  )
+        </HomeScreen>
+    )
 }
 
 const HomeScreen = styled.div`
 
 display: grid;
+margin: 2rem 0 2rem 0;
+grid-auto-rows: 12rem;
+grid-template-columns: repeat(auto-fill, minmax(11rem, 1fr));
 gap: 1rem;
-margin-bottom: 2rem;
+`;
+
+const CardContainer = styled.div`
+
+&:hover{
+
+transition: transform 0.3s ease, color 0.3s ease;
+transform: scale(1.1);
+}
 
 `;
